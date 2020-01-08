28d8e606903fa0c870df5023035b062bfb764966
Farhan Aziz <farhanazizf7@gmail.com>
2020-01-08 19:51:54 +0700

    insert search by title

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 // 1. Definisikan komponen Vue
 // Komponen "Home" akan menampilkan halaman depan aplikasi
-new Vue({
-    el: '#app',
-    data () {
+
+const Home = Vue.extend({
+    template: '#home',
+    data: function () {
+        return {
+            judul: 'Frontend Test',
+            konten: 'Vue JS with Public API',
+        }
+    }
+});
+
+const Search = Vue.extend({
+    template: '#search',
+    data: function () {
         return {
             mov: null,
             t: null,
@@ -20,10 +31,60 @@ new Vue({
                     }
                 })
                 .then(response => {
-                        this.mov = response
-                    })
+                    this.mov = response
+                })
         }
     }
 });
 
+const Title = Vue.extend({
+    template: '#title',
+    data: function () {
+        return {
+            res: null,
+            s: null,
+            i:null
+        }
+    },
+    methods: {
+        search() {
+            axios
+                .get('http://www.omdbapi.com/', {
+                    params: {
+                        apikey: '310a3fc',
+                        s: this.s
+                    }
+                })
+                .then(response => {
+                    this.res = response
+                })
+        },
+        details() {
+            axios
+                .get('http://www.omdbapi.com/', {
+                    params: {
+                        apikey: '310a3fc',
+                        i: this.i
+                    }
+                })
+                .then(response => {
+                    this.res = response
+                })
+        }
+    }
+});
+
+// 2. Definisikan routing menuju komponen
+const routes = [
+    { path: '/', component: Home },
+    { path: '/search', component: Search },
+    { path: '/title', component: Title },
+]
+
+// 3. Buat instance Router Vue
+const router = new VueRouter({ routes })
+
+// 4. Tempelkan instance "router" ke App Vue
+const zpa = new Vue({ router }).$mount('#app');
+const spa = new Vue({ router }).$mount('#top-page');
 // http://www.omdbapi.com/?apikey=310a3fc
